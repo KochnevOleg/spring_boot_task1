70c61337d7ba7f164ddec6bc84965878a2d7b1ea
KochnevOleg <95541637+KochnevOleg@users.noreply.github.com>
2023-01-15 21:03:23 +0100

    initial

---
+++ src/main/java/ru/netology/spring_boot_task1/Controller.java
@@ -0,0 +1,20 @@
+package ru.netology.spring_boot_task1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class Controller {
+    private SystemProfile profile;
+
+    public Controller(SystemProfile profile) {
+        this.profile = profile;
+    }
+
+    @GetMapping("profile")
+    public String getProfile() {
+        return profile.getProfile();
+    }
+}
---
+++ src/main/java/ru/netology/spring_boot_task1/SystemProfile.java
@@ -0,0 +1,5 @@
+package ru.netology.spring_boot_task1;
+
+public interface SystemProfile {
+    String getProfile();
+}
